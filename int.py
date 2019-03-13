1efa223ebda10192626f85a83572793624b7f6ea
Gleb <gtimurshin@student.rtc.edu>
2019-03-13 00:18:29 -0700

    int.py file added

---
+++ int.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Source: https://github.com/hzeller/rpi-rgb-led-matrix/tree/master/bindings/python
+from __future__ import absolute_import
+
+__version__ = "0.0.1"
+__author__ = "Christoph Friedrich <christoph.friedrich@vonaffenfels.de>"
+
+from .core import RGBMatrix, FrameCanvas, RGBMatrixOptions
